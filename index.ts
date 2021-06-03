a1cef1523f8f5006300beb52e2bc9e2ffe853049
sovlookup <gonorth@qq.com>
2021-06-03 20:09:27 +0800

    feat: publish ok

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
+process.chdir(process.argv[2])
 import { Framework as WebFramework } from '@midwayjs/web';
+import { Bootstrap } from '@midwayjs/bootstrap';
 const web = new WebFramework().configure({
   port: 7001,
 });
 
-import { Bootstrap } from '@midwayjs/bootstrap';
 Bootstrap.load(web).run();
